0edd60c82e63d99c8af6071079289749c72281c3
SYS-04_ACT-01 <git@github.com:shin-minhyuk/fighting-console.git>
2025-11-05 00:46:32 +0900

    FR-030 에서 ACT-01 구현 NFR-010 에서 SYS-04 구현

--- src/fighter/Fighter.java
+++ src/fighter/Fighter.java
@@ -1,4 +1,26 @@
 package fighter;
+// NFR-010
+public abstract class Fighter {
+    // 이름
+    private final String name;
+    // 체력
+    private int hp;
+    // 초기 체력 설정
+    private static final int INITIAL_HP = 100;
 
-public class Fighter {
+    public Fighter(String name) {
+        this.name = name;
+        // 초기 체력 = 100
+        this.hp = INITIAL_HP;
+    }
+    // SYS-04
+    public void resetHp() {
+        this.hp = INITIAL_HP;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public String getName() {
+        return name;
+    }
 }
--- src/fighter/PlayerFighter.java
+++ src/fighter/PlayerFighter.java
@@ -1,4 +1,39 @@
 package fighter;
 
-public class PlayerFighter {
+import java.util.Scanner;
+
+public class PlayerFighter extends Fighter {
+
+    // FR-030, ACT-01
+    private final Scanner scanner;
+
+    public PlayerFighter(String name, Scanner scanner) {
+        super(name);
+        this.scanner = scanner;
+    }
+    public String chooseAction() {
+        while (true) {
+            System.out.print("액션을 선택하세요 (1 : 주먹 \n 2 : 발차기 \n 3 : 어퍼컷 \n 4 : 로우킥 \n 5 : 피하기 ");
+
+            if (scanner.hasNextInt()) {
+                int choice = scanner.nextInt();
+
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1: return "주먹";
+                    case 2: return "발차기";
+                    case 3: return "어퍼컷";
+                    case 4: return "로우킷";
+                    case 5: return "피하기";
+                    default:
+                        System.out.println("1 부터 5 사이의 숫자를 입력해주세요!");
+                        break;
+                }
+            }else {
+                System.out.println("잘못된 입력입니다! 1 부터 5 사이의 숫자를 입력해주세요!");
+                scanner.nextLine();
+            }
+        }
+    }
 }
