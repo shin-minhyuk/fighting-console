de24de068ac43be617bff613888c5cc3bd2c4a0c
shin-minhyuk <afs_style@naver.com>
2025-11-05 17:46:48 +0900

    [TURN-02] 주사위 굴리기 + 선공자 판정 기능 추가

--- src/game/GameManager.java
+++ src/game/GameManager.java
@@ -16,9 +16,9 @@ public class GameManager {
         ConsoleUI.printIntro();
         createPlayer();
         System.out.println("\n✅초기화 완료! 게임을 시작합니다.\n");
+
+        decideFirstTurn();
     }
-
-
 
     public void createPlayer() {
         System.out.print("플레이어 이름을 입력하세요: ");
@@ -29,5 +29,28 @@ public class GameManager {
 
         player.resetHp();
         ai.resetHp();
+    }
+
+    public void decideFirstTurn() {
+        int playerDice;
+        int aiDice;
+
+        while (true) {
+            playerDice = Dice.roll(6);
+            aiDice = Dice.roll(6);
+
+            System.out.println("플레이어 주사위: " + playerDice);
+            System.out.println("AI 주사위: " + aiDice);
+
+            if (playerDice > aiDice) {
+                System.out.println("✅ 플레이어가 선공입니다!\n");
+                break;
+            } else if (playerDice < aiDice) {
+                System.out.println("🤖 AI가 선공입니다!\n");
+                break;
+            } else {
+                System.out.println("⚖️ 무승부입니다! 다시 주사위를 굴립니다...\n");
+            }
+        }
     }
 }
