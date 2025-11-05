d9f51c237d1f2ddcb8de8b6b1697720031033088
gongyoonkyung <rorakong03@gmail.com>
2025-11-05 10:38:06 +0900

    TURN-01 구현

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -1,5 +1,16 @@
 package game;
 
+import java.util.Random;
+
 public class Dice {
 
+    private static final Random random = new Random();
+
+    // TURN-01
+    public static int roll(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("🎲");
+        }
+        return random.nextInt(n) + 1;
+    }
 }
